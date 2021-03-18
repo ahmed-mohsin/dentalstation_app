a0c82f5b35520e7cd29db94e82afe14c901a0bc2
ahmed <ph.ahmedmohsin@gmail.com>
2021-03-18 21:50:46 +0200

    first commit

---
+++ lib/productsjson.dart
@@ -0,0 +1,42 @@
+List productList =[
+{
+"id": "1",
+"name": "Nsk S Max Pico",
+"description": "The innovative air turbine with ultra mini head and Super Slim body.The NsK S-Max pico Series has been developed for the operation of Minimal Intervention (M.I.). The ultra mini head and Super & Slim handpiece design is the result of fine NsK precision engineering, allowing a wider view of the operational field and greater flexibility for dexterity.",
+"url": "https://images.dentalkart.com/media/catalog/product/n/s/nsk_s-max1.jpg",
+"category": "Equipments",
+"title": "Optic handpiece",
+"price": 550,
+"oldPrice": 300
+},
+{
+"id": "2",
+"name": "Ivoclar Bluephase N",
+"description": "By covering the complete wavelength range and providing a light intensity of 1,200 mW/cm2, the LED curing light Bluephase N sets highest standards. It meets all the demands of day-to-day dental procedures thanks to its many innovations.",
+"url": "https://images.dentalkart.com/media/catalog/product/u/p/up.jpg",
+"category": "Equipments",
+"title": "LED polymerization light",
+"price": 15442,
+"oldPrice": 10000
+},
+{
+"id": "3",
+"name": "Apple Straight Handpiece",
+"description": "Intended for dental clinical treatment and preventionSuitable for any E-type headpiece accords with ISO standard Suitable for both steam and chemical autoclave",
+"url": "https://images.dentalkart.com/media/catalog/product/a/p/appairotor.jpg",
+"category": "Equipments",
+"title": "Straight Handpiece",
+"price": 500,
+"oldPrice": 300
+},
+{
+"id": "4",
+"name": "Being Foshan Contra Angle Handpiece",
+"description": "BEING FOSHAN CONTRA HANDPIECE is a E-type low speed handpiece with wrench type",
+"url": "https://images.dentalkart.com/media/catalog/product/c/o/cont5.png",
+"category": "category 4",
+"title": "Contra Angle Handpiece",
+"price": 200,
+"oldPrice": 256
+}
+];
